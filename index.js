c2b6c705d2c9ac4d9a7b9a4ec8ae7374cb2cdae8
dileep <ddr.dileepraloti@gmail.com>
2023-11-28 17:37:56 +0530

    fix : index file

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import APP from "./src/app.js";
 const port = process.env.APP_PORT || 9001;
 
 APP.listen(port, () => {
-  console.log("App is running on port " + port);
+  console.log(`App is running on port http://localhost:${port}`);
 });
 
